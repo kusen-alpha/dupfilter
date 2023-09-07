a984c64bfbbf44ad36c20e4cd2a1f1c2901f0564
huguansheng <hu119454>
2023-09-07 15:17:35 +0800

    RedisSetFilter增加自动清除逻辑

--- dupfilter/filters/redis/setfilter.py
+++ dupfilter/filters/redis/setfilter.py
@@ -3,6 +3,10 @@
 # email: 1194542196@qq.com
 # date: 2023/9/6
 
+import sys
+
+import cachetools
+
 from dupfilter.filters.redis import RedisFilter
 
 EXISTS_SCRIPT = """
@@ -36,17 +40,32 @@ for index, key in pairs(keys) do
 end
 return result
 """
+RESET_SCRIPT = """
+local key = KEYS[1]
+local count = ARGV[1]
+redis.call('SPOP', key, count)
+return true
+"""
 
 
 class SetFilter(RedisFilter):
-    def __init__(self, server, key, block_num=1, *args, **kwargs):
+    def __init__(self, server, key, block_num=1, maxsize=sys.maxsize,
+                 reset=False, reset_proportion=0.8, reset_check_period=7200,
+                 *args, **kwargs):
         self.key = key
         self.block_num = block_num
+        self.maxsize = maxsize
+        self.reset = reset
+        self.reset_proportion = reset_proportion
+        self.reset_check_period = reset_check_period
         super(SetFilter, self).__init__(server, *args, **kwargs)
         self.exists_script = self.server.register_script(EXISTS_SCRIPT)
         self.insert_script = self.server.register_script(INSERT_SCRIPT)
         self.exists_and_insert_script = self.server.register_script(
             EXISTS_AND_INSERT_SCRIPT)
+        self.reset_script = self.server.register_script(RESET_SCRIPT)
+        self.reset_info = cachetools.TTLCache(maxsize=2, ttl=reset_check_period)
+        self.reset_info['proportions'] = self.proportions
 
     def exists(self, value):
         return self.exists_many([value])
@@ -71,6 +90,25 @@ class SetFilter(RedisFilter):
     def insert_many(self, values):
         keys, values = self._get_keys_and_values(values)
         return self.insert_script(keys=keys, args=values)
+
+    def _reset(self, count):
+        proportions = self.reset_info.get('proportions')
+        if not proportions:
+            self.reset_info['proportions'] = self.proportions
+        key, proportion = sorted(
+            list(self.reset_info['proportions'].items()),
+            key=lambda x: x[1])[-1]
+        if proportion < self.reset_proportion:
+            return
+        self.reset_script(keys=[key], args=[count])
+
+    @property
+    def proportions(self):
+        proportions = {}
+        for block in range(self.block_num):
+            key = self.key + str(block)
+            proportions[key] = self.server.scard(key) / self.maxsize
+        return proportions
 
     def _get_keys_and_values(self, values):
         values = [self._value_hash(value) for value in values]
